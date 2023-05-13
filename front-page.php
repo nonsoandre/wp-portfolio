f7fb7ebb11acb03b05a6f00e2a0d49b0da2014cc
Andre Dotti <nonsoandre@gmail.com>
2023-05-13 03:39:11 +0100

    added blog url

--- front-page.php
+++ front-page.php
@@ -85,7 +85,7 @@
             </div>
           </div> -->
 
-          <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+          <p class="t-center no-margin"><a href="<?php echo site_url("/blog")?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
       </div>
     </div>
