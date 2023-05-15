afea13e5ea220032fd5f3e741299f3dc82c98676
Andre Dotti <nonsoandre@gmail.com>
2023-05-15 06:05:15 +0100

    setup gulp most of the changes werre test to see for gulp setup

--- functions.php
+++ functions.php
@@ -2,16 +2,14 @@
 
 function portfolioFiles(){
     wp_enqueue_style("font-awesome", "//use.fontawesome.com/releases/v5.0.7/css/all.css");
-
     wp_enqueue_style("google_font", "//fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap");
 
     wp_enqueue_style("index-css", get_template_directory_uri()."/assets/build/index.css");
     wp_enqueue_style("style-index-css", get_template_directory_uri()."/assets/build/style-index.css");
     wp_enqueue_style("my-css", get_template_directory_uri()."/assets/build/my.css");
-    
-    wp_enqueue_script("main.js", get_theme_file_uri("/assets/build/index.js"), array("jquery"), "1.0", true);
 
-    // wp_enqueue_style( "wp-style", get_stylesheet_uri(), NULL , "1.0", true );
+    wp_enqueue_script("main.js", get_theme_file_uri("/assets/build/index.js"), array("jquery"), "1.0", true);
+    wp_enqueue_style( "wp-style", get_stylesheet_uri(), NULL , "1.0", true );
 };
 
 add_action("wp_enqueue_scripts", "portfolioFiles");
