dd4dc044d52b2828236c968deea5025b13d6d543
Andre Dotti <nonsoandre@gmail.com>
2023-05-18 03:35:39 +0100

    created the past events page and setup pagination for custom query cases

--- page-past-events.php
+++ page-past-events.php
@@ -4,9 +4,9 @@
     <div class="page-banner">
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/assets/images/ocean.jpg") ?>)"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">All Events</h1>
+            <h1 class="page-banner__title">Past Events</h1>
             <div class="page-banner__intro">
-                <p>Stay Updated with the latest Events 
+                <p>Recap of Events 
                     <?php                     
                         if(is_category()) {
                             echo "for "; single_cat_title();
@@ -23,22 +23,30 @@
 
     <div class="container container--narrow page-section">
 <?php 
+
+//the get_query_var functions queries the value of the url and returns a page number in the url. So what we are saying is that if there is a page number return it or if not default to 1
+
+$pastEventPageCustomQueryForEvents = new WP_Query(array(
+    "paged" => get_query_var('paged', 1),
+    "post_type" => "event",
+    "posts_per_page" => 1,
+    "meta_key" => "event_date",
+    "orderby" => 'meta_value_num',
+    "order" => "ASC",
+    "meta_query" => array(
+      array(
+        "key" => "event_date",
+        "compare" => "<",
+        "value" => date("Ymd")
+      )
+    )
+  ));
     // looping in wordpress
-    while(have_posts()) {
-        the_post();
+    while($pastEventPageCustomQueryForEvents->have_posts()) {
+        $pastEventPageCustomQueryForEvents->the_post();
 
 ?>
-    <!-- <div class="event-summary">
-        <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month"><?php the_time("M") ?></span>
-            <span class="event-summary__day"><?php the_time("d") ?></span>
-        </a>
-        <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink()?>"><?php the_title() ?></a></h5>
-            <p><?php  echo wp_trim_words(get_the_excerpt(), 18); ?><a href="<?php the_permalink()?>" class="nu gray">Learn more</a></p>
-        </div>
-    </div> -->
-    <div class="event-summary">
+          <div class="event-summary">
             <a class="event-summary__date t-center" href="#">
               <span class="event-summary__month">
                 <?php 
@@ -59,18 +67,19 @@
               }else{
                 echo wp_trim_words(get_the_content(), 18);
               }
-              ?><a href="<?php the_permalink()?>" class="nu gray">Learn more</a></p>
+              ?> ...<a href="<?php the_permalink()?>" class="nu gray">Learn more</a></p>
             </div>
           </div>
+
 <?php } ?>
 
 <?php 
     // wp-pagination
-    echo paginate_links();
+    echo paginate_links(array(
+        'total' => $pastEventPageCustomQueryForEvents->max_num_pages
+    ));
 
 ?>
-    <hr class="section-break">
-    <p>Looking for a past event recap? Checkout our past event archive <a href="<?php echo site_url("/past-events"); ?>">Past Events</a></p>
     </div>
 
 
