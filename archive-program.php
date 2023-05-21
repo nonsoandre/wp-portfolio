e1ab90c3d1ebff1e2d205bd9bc7d91321b3dedf6
Andre Dotti <nonsoandre@gmail.com>
2023-05-21 15:33:51 +0100

    added program post type and all its corresponding pages.

--- archive-program.php
+++ archive-program.php
@@ -4,9 +4,9 @@
     <div class="page-banner">
         <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri("/assets/images/ocean.jpg") ?>)"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">All Projects</h1>
+            <h1 class="page-banner__title">All Programs</h1>
             <div class="page-banner__intro">
-                <p>Latest Projects done by me 
+                <p>All programs offered by our university
                     <?php                     
                         if(is_category()) {
                             echo "for "; single_cat_title();
--- header.php
+++ header.php
@@ -33,7 +33,7 @@
     // ));
 ?>
               <li <?php if (is_page('about-us') or wp_get_post_parent_id(get_the_ID() == 7)) echo "class='current-menu-item'";  ?>><a href="<?php echo site_url("/about-us"); //the site_url function calls the root of your webiste path ?>">About Us</a></li>
-              <li><a href="#">Programs</a></li>
+              <li <?php if(get_post_type() == "program") echo "class='current-menu-item'"; ?> ><a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a></li>
               <li <?php if(get_post_type() == "event" OR is_page('past-events')) echo "class = 'current-menu-item' "?>><a href="<?php echo site_url("/events")?>">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li <?php if (get_post_type() == "post") echo "class='current-menu-item'";  ?>><a href="<?php echo site_url("/blog")?>">Blog</a></li>
