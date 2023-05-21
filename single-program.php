bdd4de137d94fa82c27373f41ec0052048ce5eaa
Andre Dotti <nonsoandre@gmail.com>
2023-05-21 16:35:22 +0100

    back links for the related programs.

--- single-program.php
+++ single-program.php
@@ -32,6 +32,72 @@ while(have_posts()) {
         <div class="generic-content">
             <?php the_content(); ?>
         </div> 
+
+        <div class="related-programs">
+            <hr class="section-break">
+            <h2>Related Event(s)</h2>
+
+<?php 
+$pastEventPageCustomQueryForEvents = new WP_Query(array(
+    "paged" => get_query_var('paged', 1),
+    "post_type" => "event",
+    "posts_per_page" => 1,
+    "meta_key" => "event_date",
+    "orderby" => 'meta_value_num',
+    "order" => "ASC",
+    "meta_query" => array(
+      array(
+        "key" => "event_date",
+        "compare" => ">=",
+        "value" => date("Ymd")
+      ),
+      array(
+        "key" => "related_programs", //post type name you want to work on
+        "compare" => "LIKE",
+        "value" => '"' . get_the_ID() . '"'
+      )
+    )
+  ));
+    // looping in wordpress
+    while($pastEventPageCustomQueryForEvents->have_posts()) {
+        $pastEventPageCustomQueryForEvents->the_post();
+
+?>
+          <div class="event-summary">
+            <a class="event-summary__date t-center" href="#">
+              <span class="event-summary__month">
+                <?php 
+                    $eventTime = new DateTime(get_field('event_date'));
+                    echo $eventTime->format('M');
+                ?>
+              </span>
+              <span class="event-summary__day">                <?php 
+                    $eventTime = new DateTime(get_field('event_date'));
+                    echo $eventTime->format('d');
+                ?>
+              </span>
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink()?>"><?php the_title() ?></a></h5>
+              <p><?php if(has_excerpt()) {
+                  echo get_the_excerpt();
+              }else{
+                echo wp_trim_words(get_the_content(), 18);
+              }
+              ?> ...<a href="<?php the_permalink()?>" class="nu gray">Learn more</a></p>
+            </div>
+          </div>
+
+<?php } ?>
+
+<?php 
+    // wp-pagination --> how to setup for custom queries
+    echo paginate_links(array(
+        'total' => $pastEventPageCustomQueryForEvents->max_num_pages
+    ));
+
+?>
+        </div>
     </div>
 
 <?php } get_footer(); ?>`
