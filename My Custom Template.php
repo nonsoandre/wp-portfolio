e599f4322f5395ef70136b13a196f4e42d944fe0
Andre Nonso <jamesbonde09@gmail.com>
2023-06-02 04:55:49 +0100

    test commit

--- My Custom Template.php
+++ My Custom Template.php
@@ -5,5 +5,9 @@ Template Name: My Custom Template
 
 ?>
 
+<p>Hello world</p>
+
+I need to know 
+
 This is a custom template to use in adding your custom pages to wordpress so that it can be selecatable in the templates drop down.
 Basically you can build your blog templates and other page templates here.
