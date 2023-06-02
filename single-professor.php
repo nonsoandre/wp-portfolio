e6b0b97319d07b6be5472214403dd3768f255015
Andre Nonso <jamesbonde09@gmail.com>
2023-06-02 04:45:58 +0100

    commit from new laptop

--- single-professor.php
+++ single-professor.php
@@ -3,22 +3,11 @@
 
 while(have_posts()) {
     the_post();
+
+    pageBannerTemplate();
 ?>
 
-
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImg = get_field('page_cover_image'); echo $pageBannerImg['url']; ?>)"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
-            <div class="page-banner__intro">
-            <p><?php the_field('page_banner_subtitle'); ?> </p>
-            </div>
-        </div>
-    </div>
-
-
-
-    <div class="container container--narrow page-section">    
+   <div class="container container--narrow page-section">    
         <!-- START META BOX INNER PAGE -->
         <!-- <div class="metabox metabox--position-up metabox--with-home-link">
             <p>
